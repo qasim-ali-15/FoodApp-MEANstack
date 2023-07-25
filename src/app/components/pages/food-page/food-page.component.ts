d917f01aa02d3c2d13721f7a7a4fccde56e1286c
Qasim ALi <119096357+qasim-ali-15@users.noreply.github.com>
2023-07-25 11:57:29 +0500

    refactroing

--- src/app/components/pages/food-page/food-page.component.ts
+++ src/app/components/pages/food-page/food-page.component.ts
@@ -34,4 +34,8 @@ export class FoodPageComponent implements OnInit {
     this.cartService.addToCart(this.food);
     this.router.navigateByUrl('/cart-page');
   }
+
+  favoriteFood(){
+    this.food.favorite = !this.food.favorite;
+  }
 }
